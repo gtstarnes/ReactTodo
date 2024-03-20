eb124a3fd38deaa0f43d16ddf11c1d458b689a79
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-19 20:24:19 -0400

    created initial state for error message

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -4,6 +4,7 @@ import { TaskList } from "../Types/types"
 const TodoList = () => {
     const [tasks, setTasks] = useState<TaskList[]>([])
     const [newTask, setNewTask] = useState<string>('')
+    const [error, setError] = useState<string>('')
   return (
     <div>
         <div>
